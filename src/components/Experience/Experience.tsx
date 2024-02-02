756164283f179bc0a8614ec0388661ac1d817d4d
SatiNeeraj <neeraj.sati@tamasha.live>
2024-02-02 22:47:10 +0530

    fix: update experience section

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -9,11 +9,21 @@ function Experience() {
         </div>
         <div className='subDesc'>
             <div className='schoolName'>
-            Tamasha.live (Gamepe Technologies)
+                ASL (Unischolars, UniCreds, Uniacco)
+            </div>
+            <div className='degreeName nopadding'>
+                <p>Frontend Developer</p>
+                <p>August 2023 - Present</p>
+            </div>
+            <div className='degreeName'>
+                Next Js, React Js, Redux
+            </div>
+            <div className='schoolName'>
+                Tamasha.live (Gamepe Technologies)
             </div>
             <div className='degreeName nopadding'>
                 <p>Web Developer Intern</p>
-                <p>August 2022 - Present</p>
+                <p>August 2022 - Jun 2023</p>
             </div>
             <div className='degreeName'>
             Node Js, Express Js, PostgreSQL, Redis
